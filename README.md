abfeea12a06ebf6408ad94adc029205e1c908be2
rebeca <rbcmas@gmail.com>
2017-06-28 09:53:16 +0200

    added README_IMPORTANT

--- README.md
+++
@@ -1,2 +0,0 @@
-# TFM_RS_MasterData
-Welcome!!! This is my final work of the master Data Science ;D
---
+++ README_IMPORTANT.py
@@ -0,0 +1,27 @@
+"""
+ESQUEMA CONTENIDOS FUNCIONALES:
+
+A continuación de describen los archivos y organización de los contenidos en el siguiente proyecto.
+
+RSmodule.py ----->>>
+Este es el archivo más importante y es el que ejecuta el recomendador.
+Ejecuta las funciones anexas en el resto del proyecto y  que va importando.
+Contiene la lógica del negocio.
+
+RSmodule_test.py ----->>>
+Mismo archivo con pruebas varias.
+
+DataJoin.py ----->>>
+Contine varias funciones, para cargar archivos, y hacer joins
+
+Scores_test.py ----->>>
+
+Kmeand_test.py ----->>>
+
+Kmeand_super_simple.py ----->>>
+
+
+
+
+
+"""
